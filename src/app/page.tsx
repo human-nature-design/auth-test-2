9dc2a62afe9d0640abc970e13cc0173a1e7cd1f1
human-nature-design <58803707+human-nature-design@users.noreply.github.com>
2025-10-11 01:25:20 -0700

    Fix todo redirect to protected route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const { data: { user } } = await supabase.auth.getUser()
 
   if (user) {
-    redirect('/todo-3')
+    redirect('/(protected)/todo-3')
   }
 
   return (
